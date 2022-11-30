2f4288cfa80ef44a9dfcd8489d8908fb4228f4c7
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2022-11-30 20:34:09 +0200

    Add: Class Dwarf

---
+++ src/fundamental/Dwarf.java
@@ -0,0 +1,64 @@
+package fundamental;
+
+import factory.BikeFactory;
+import factory.DollFactory;
+import factory.Toy;
+
+public class Dwarf implements IObserver {
+    private final BikeFactory bikeFactory;
+    private final DollFactory dollFactory;
+    private String name;
+    private IObservable observedWishBoard;
+
+    public Dwarf(String name) {
+        setName(name);
+        dollFactory = new DollFactory();
+        bikeFactory = new BikeFactory();
+    }
+
+    @Override
+    public void update() {
+        if (observedWishBoard == null) {
+            throw new IllegalArgumentException("There isn`t a wish board specified!");
+        }
+
+        Toy toyPreparedForSanta = null;
+        String writtenToy = observedWishBoard.getToy();
+
+        switch (writtenToy) {
+            case "barbie doll" -> toyPreparedForSanta = dollFactory.produceAToy("barbie boll");
+            case "wooden doll" -> toyPreparedForSanta = dollFactory.produceAToy("wooden doll");
+            case "folding bike" -> toyPreparedForSanta = bikeFactory.produceAToy("folding bike");
+            case "mountain bike" -> toyPreparedForSanta = bikeFactory.produceAToy("mountain bike");
+            case "road bike" -> toyPreparedForSanta = bikeFactory.produceAToy("road bike");
+            case "touring bike" -> toyPreparedForSanta = bikeFactory.produceAToy("touring bike");
+            default -> System.out.println("Not an existing type of a toy!");
+        }
+
+        if (toyPreparedForSanta == null) {
+            throw new IllegalArgumentException("This item cannot be null!");
+        }
+    }
+
+    @Override
+    public void setWishBoard(IObservable wishBoard) {
+        if (wishBoard == null) {
+            throw new IllegalArgumentException("The wish board this dwarf observes cannot be null!");
+        }
+        observedWishBoard = wishBoard;
+    }
+
+    @Override
+    public void removeWishBoard() {
+        observedWishBoard = null;
+
+    }
+
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("The name of the dwarf cannot be null or blank!");
+        }
+        this.name = name;
+    }
+}
