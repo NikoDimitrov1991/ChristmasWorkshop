18f65ed68e4dd5de654359528ab3cdc06c0c001f
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2022-12-01 18:53:49 +0200

    Add: application data in Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import fundamental.Dwarf;
+import fundamental.Santa;
+import fundamental.WishBoard;
+
 public class Main {
+    public static Santa santaInstance = Santa.getInstance();
+    static class Workshop {
+
+        public WishBoard wishBoard;
+
+        public Dwarf dwarf;
+
+        public Workshop(String dwarfName) {
+            wishBoard = new WishBoard();
+            dwarf = new Dwarf(dwarfName);
+            wishBoard.subscribe(dwarf);
+        }
+
+        public WishBoard getWishBoard() {
+            return wishBoard;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Workshop santasWorkshop = new Workshop("Gosho");
+        santaInstance.requestAToy("wooden doll", santasWorkshop.getWishBoard());
+        santaInstance.requestAToy("barbie doll", santasWorkshop.getWishBoard());
+        santaInstance.requestAToy("mountain bike", santasWorkshop.getWishBoard());
+        santaInstance.requestAToy("touring bike", santasWorkshop.getWishBoard());
+        santaInstance.requestAToy("road bike", santasWorkshop.getWishBoard());
+        santaInstance.showAllToys();
     }
 }
